2ef21f182df7db0d75c141955a7fbb614a30d887
mafcmx <marcofariasmx@gmail.com>
2021-10-31 15:20:10 -0600

    first semantic cube added

---
+++ semantics.py
@@ -0,0 +1,129 @@
+class Semantic():
+    
+    def __init__(self):
+
+        self.semantic_cube = {
+            'int': {
+                '+': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '-': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '=': {
+                    'int': 'int',
+                    'float': 'int'
+                },
+                '*': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '/': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '<': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '<=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '==': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '!=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                }
+            },
+
+            'float': {
+                '+': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '-': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '=': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '*': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '/': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '<': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '<=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '==': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '!=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                }
+            },
+
+            'char': {
+                '=': {
+                    'char': 'char'
+                },
+                '==': {
+                    'char': 'bool'
+                },
+                '!=': {
+                    'char': 'bool'
+                }
+            },
+
+            'bool': {
+                '=': {
+                    'bool': 'bool'
+                },
+                '==': {
+                    'bool': 'bool'
+                },
+                '!=': {
+                    'bool': 'bool'
+                },
+                '&': {
+                    'bool': 'bool'
+                },
+                '|': {
+                    'bool': 'bool'
+                }
+            }
+        }
